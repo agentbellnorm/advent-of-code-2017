8c09cbc0eeb5ecd0b6678afaad076cf3d843182a
agentbellnorm <morgan.bentell@gmail.com>
2017-12-24 02:46:09 +0100

    day 7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject day02 "0.1.0-SNAPSHOT"
+(defproject advent-of-code-2017 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
